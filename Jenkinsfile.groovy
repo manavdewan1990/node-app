610b2421091e502ff17a67c68612c9a6bfdc5c40
manavd <manavd@hotstar.com>
2018-09-30 09:48:17 +0530

    updated jenkin

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -44,7 +44,7 @@ pipeline {
                     sh '''
                cd node-app-terraform
                terraform init -var access_key=${AWS_KEY} -var secret_key=${AWS_SECRET} -backend-config="profile=default"
-               terraform apply -auto-approve -var access_key=${AWS_KEY} -var secret_key=${AWS_SECRET}
+               terraform destroy -force -auto-approve -var access_key=${AWS_KEY} -var secret_key=${AWS_SECRET}
                ls
 
             '''
